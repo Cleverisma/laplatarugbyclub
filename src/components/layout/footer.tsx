4d6006cf8661b401dc83fee51ffd50c42d12bc4b
Seba Cardoso <sebastiancardoso92@gmail.com>
2026-03-15 20:53:13 -0300

    Add new sponsors to footer component and update image filters for improved visibility

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -4,6 +4,8 @@ import sponsorGatorade from '~/media/sponsors/Gatorade.png?url';
 import sponsorFlash from '~/media/sponsors/flash.svg?url';
 import sponsorMacro from '~/media/sponsors/macro.svg?url';
 import sponsorPlusmar from '~/media/sponsors/plusmar.png?url';
+import sponsorStella from '~/media/sponsors/stella.svg?url';
+import sponsorAon from '~/media/sponsors/aon.svg?url';
 
 export const Footer = component$(() => {
   return (
@@ -17,16 +19,22 @@ export const Footer = component$(() => {
         <div class="container mx-auto px-4 max-w-7xl">
           <div class="flex flex-wrap justify-center items-center gap-12 md:gap-20 lg:gap-28">
             <a href="#" class="group" aria-label="Sponsor Banco Macro">
-              <img src={sponsorMacro} alt="Banco Macro" width="160" height="64" class="h-10 md:h-14 lg:h-16 w-auto object-contain filter grayscale opacity-70 group-hover:grayscale-0 group-hover:opacity-100 transition-all duration-300" />
+              <img src={sponsorMacro} alt="Banco Macro" width="160" height="64" class="h-10 md:h-14 lg:h-16 w-auto object-contain filter brightness-0 invert opacity-70 group-hover:brightness-100 group-hover:invert-0 group-hover:opacity-100 transition-all duration-300" />
+            </a>
+            <a href="#" class="group" aria-label="Sponsor Stella Artois">
+              <img src={sponsorStella} alt="Stella Artois" width="160" height="64" class="h-10 md:h-14 lg:h-16 w-auto object-contain filter brightness-0 invert opacity-70 group-hover:brightness-100 group-hover:invert-0 group-hover:opacity-100 transition-all duration-300" />
             </a>
             <a href="#" class="group" aria-label="Sponsor Flash">
-              <img src={sponsorFlash} alt="Flash" width="160" height="64" class="h-10 md:h-14 lg:h-16 w-auto object-contain filter grayscale opacity-70 group-hover:grayscale-0 group-hover:opacity-100 transition-all duration-300" />
+              <img src={sponsorFlash} alt="Flash" width="160" height="64" class="h-10 md:h-14 lg:h-16 w-auto object-contain filter brightness-0 invert opacity-70 group-hover:brightness-100 group-hover:invert-0 group-hover:opacity-100 transition-all duration-300" />
             </a>
             <a href="#" class="group" aria-label="Sponsor Gatorade">
-              <img src={sponsorGatorade} alt="Gatorade" width="160" height="64" class="h-10 md:h-14 lg:h-16 w-auto object-contain filter grayscale opacity-70 group-hover:grayscale-0 group-hover:opacity-100 transition-all duration-300" />
+              <img src={sponsorGatorade} alt="Gatorade" width="160" height="64" class="h-10 md:h-14 lg:h-16 w-auto object-contain filter brightness-0 invert opacity-70 group-hover:brightness-100 group-hover:invert-0 group-hover:opacity-100 transition-all duration-300" />
             </a>
             <a href="#" class="group" aria-label="Sponsor Plusmar">
-              <img src={sponsorPlusmar} alt="Plusmar" width="160" height="64" class="h-10 md:h-14 lg:h-16 w-auto object-contain filter grayscale opacity-70 group-hover:grayscale-0 group-hover:opacity-100 transition-all duration-300" />
+              <img src={sponsorPlusmar} alt="Plusmar" width="160" height="64" class="h-10 md:h-14 lg:h-16 w-auto object-contain filter brightness-0 invert opacity-70 group-hover:brightness-100 group-hover:invert-0 group-hover:opacity-100 transition-all duration-300" />
+            </a>
+            <a href="#" class="group" aria-label="Sponsor AON">
+              <img src={sponsorAon} alt="AON" width="160" height="64" class="h-10 md:h-14 lg:h-16 w-auto object-contain filter brightness-0 invert opacity-70 group-hover:brightness-100 group-hover:invert-0 group-hover:opacity-100 transition-all duration-300" />
             </a>
           </div>
         </div>
